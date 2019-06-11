06291d4d326225fbd88c57c78a51c50245bcf27f
CleanestMink126 <gns126@gmail.com>
2019-06-11 09:43:50 -0400

    Converted the save files into JSON formatting

--- Clew/FeedbackLogger.swift
+++ Clew/FeedbackLogger.swift
@@ -19,12 +19,15 @@ class FeedbackLogger {
     func writeFeedbackToFirebase(name : String, data : Data) -> Int {
         //creates an intiger which stores the return value of the function (0 for no errors and 1 for a failed build)
         var returnValue = 0
-        //creates a reference to the location we want to save the new file
-        let fileRef = feedbackRef.child("\(name) \(UUID().uuidString)")
         
+        //creates a reference to the location we want to save the new file
+        let fileRef = feedbackRef.child("\(name)_\(UUID().uuidString)_metadata.json")
+        
+        let fileType = StorageMetadata()
+        fileType.contentType = "application/json"
         
         // Upload the file to the path defined by fileRef then checks for any errors
-        let uploadTask = fileRef.putData(data, metadata: nil){ (metadata, error) in
+        let uploadTask = fileRef.putData(data, metadata: fileType){ (metadata, error) in
             guard metadata != nil else {
                 // prints an errorstatement to the console
                 print("could not upload feedback to firebase", error!.localizedDescription)
@@ -38,7 +41,8 @@ class FeedbackLogger {
     }
     
     //takes in all the different pieces of data and combines them into a properly formatted string of type Data
-    func makeData(name: String, message: String, country inputCountry: String?, phone: String, email: String) -> Data {
+    func makeData(name: String, message: String, country inputCountry: String?, phone: String, email: String) -> Data? {
+        
         var country: String? = "NONE"
         
         //performs input processing on the country Field
@@ -48,19 +52,25 @@ class FeedbackLogger {
             country = inputCountry
         }
         
-        //combines all of the input strings into one data string of the proper formatting
-        let data = "DATE: \(Date().description(with: .current)) \nPHONE NUMBER: \(phone) \nEMAIL: \(email) \nNAME: \(name) \nCOUNTRY: \(country!) \nMESSAGE: \(message)"
-        
-        //converts the data into a NSData type so it can be pushed to firebase
-        let feedbackNSString = data as NSString
-        let feedbackData = feedbackNSString.data(using: String.Encoding.utf8.rawValue)!
-        
-        //returns the data properly combined and formatted
-        return feedbackData
+        //places the data into a dictionary to be formatted into JSON later
+        let body: [String : Any] = ["FeedbackDate": "\(Date().description(with: .current))",
+                                    "PhoneNumber": phone,
+                                    "Email": email,
+                                    "Name": name,
+                                    "Country": country,
+                                    "Message": message]
+        do {
+            let data = try JSONSerialization.data(withJSONObject: body, options: .prettyPrinted)
+            //returns the data properly combined and formatted
+            return data
+        }catch {
+        print(error.localizedDescription)
+        }
+        return nil
     }
     //combines the information together and saves a file to Firebase containing the user's feedback
     func saveFeedback(name: String, message: String, country: String?, phone: String, email: String) -> Int{
         //calls the functions to create the properly formatted data and upload the result to firebase
-        return writeFeedbackToFirebase(name: name, data: makeData(name: name, message: message, country: country, phone: phone, email: email))
+        return writeFeedbackToFirebase(name: name, data: makeData(name: name, message: message, country: country, phone: phone, email: email)!)
     }
 }
