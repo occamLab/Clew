9fc940c116d1591d5d1d3a22cba92f592f5fc86f
tnovak-olin <tnovak@olin.edu>
2019-06-25 12:02:05 -0400

    fixed accessability current version is broken because javascript does not compile

---
+++ help/javascript/helpDocsJavascript.js
@@ -0,0 +1,40 @@
+//finds all of the collapseable elements in the menu
+var coll = document.getElementsByClassName("collapsible");
+//creates an index counter variable
+var i;
+
+function updateAccessabilityLabels (htmlElement,action){
+    if action == "open"{
+        //change the accessability tag
+        //htmlElement.setAttribute("aria-Label","expand " + htmlElement.innerHTML + " section")
+    }else{
+        //change the accessability tag
+        //htmlElement.setAttribute("aria-Label","collapse " + htmlElement.innerHTML + " section")
+    }
+    return 0
+}
+
+
+//itterates through al of the collapseable elements
+for (i = 0; i < coll.length; i++) {
+//adds clickable functionality
+coll[i].addEventListener("click", function() {
+                         //toggles the active state of the collapseable element
+                         this.classList.toggle("active");
+                         //grabs a reference to the content
+                         var content = this.nextElementSibling;
+                         //if the content is displayed
+                         if (content.style.display === "block") {
+                         //hide the content
+                         content.style.display = "none";
+                         //change the accessability tag
+                         //updateAccessabilityLabels(this,"close");
+                         
+                         } else {
+                         //show the content
+                         content.style.display = "block";
+                         //change the accessability tag
+                         //updateAccessabilityLabels(this,"open");
+                         }
+                         });
+}
