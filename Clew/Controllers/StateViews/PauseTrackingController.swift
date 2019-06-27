8e24c7062ef0b06de0744f912357d9f0c5471494
Dieter Brehm <inkering@gmail.com>
2019-06-27 14:52:05 -0400

    Implement Dynamic Type and constraint layout system
    implement apple's dynamic type system, reflowing scroll views for label,
    and constraint based button layout prototype.

--- Clew/Controllers/StateViews/PauseTrackingController.swift
+++ Clew/Controllers/StateViews/PauseTrackingController.swift
@@ -22,7 +22,7 @@ class TransparentTouchView: UIView {
 
 /// A View Controller for handling the pause route state
 /// also handles associated buttons
-class PauseTrackingController: UIViewController {
+class PauseTrackingController: UIViewController, UIScrollViewDelegate {
 
     /// button for storing landmark descriptions
     var enterLandmarkDescriptionButton: UIButton!
@@ -33,48 +33,114 @@ class PauseTrackingController: UIViewController {
     
     /// button for aligning phone position in space
     var confirmAlignmentButton: UIButton!
-
+    
+    var label: UILabel!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+    }
+    
     /// called when the view has loaded.  We setup various app elements in here.
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        /// create a main view which passes touch events down the hierarchy
         view = TransparentTouchView(frame:CGRect(x: 0,
                                                  y: 0,
                                                  width: UIScreen.main.bounds.size.width,
                                                  height: UIScreen.main.bounds.size.height))
-
-        let label = UILabel(frame: CGRect(x: 15,
-                                          y: UIScreen.main.bounds.size.height/5,
-                                          width: UIScreen.main.bounds.size.width-30,
-                                          height: UIScreen.main.bounds.size.height/2))
         
+        /// create a label, and a scrollview for it to live in
+        label = UILabel()
+        let scrollView = UIScrollView()
+        
+        /// allow for constraints to be applied to label, scrollview
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.indicatorStyle = .white;
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        /// darken background of view
         view.backgroundColor = UIColor.black.withAlphaComponent(0.4)
         
+        /// label details
         let waitingPeriod = ViewController.alignmentWaitingPeriod
         let mainText = String.localizedStringWithFormat(NSLocalizedString("Landmarks allow you to save or pause your route. You will need to return to the landmark to load or unpause your route. Before creating the landmark, specify text or voice to help you remember its location. To create a landmark, hold your device flat with the screen facing up. Press the top (short) edge flush against a flat vertical surface (such as a wall).  The \"align\" button starts a %lu-second countdown. During this time, do not move the device.", comment: "Info for user"), waitingPeriod)
         label.textColor = UIColor.white
         label.textAlignment = .center
         label.numberOfLines = 0
         label.lineBreakMode = .byWordWrapping
-        label.font = label.font.withSize(20)
+        label.font = UIFont.preferredFont(forTextStyle: .body)
         label.text = mainText
         label.tag = UIView.mainTextTag
-        view.addSubview(label)
         
-        enterLandmarkDescriptionButton = UIButton.makeImageButton(view,
+        /// place label inside of the scrollview
+        scrollView.addSubview(label)
+        view.addSubview(scrollView)
+
+        /// set top, left, right constraints on scrollView to
+        /// "main" view + 8.0 padding on each side
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100.0).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8.0).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8.0).isActive = true
+        
+        /// set the height constraint on the scrollView to 0.5 * the main view height
+        scrollView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        
+        /// set top, left, right AND bottom constraints on label to
+        /// scrollView + 8.0 padding on each side
+        label.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 8.0).isActive = true
+        label.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 8.0).isActive = true
+        label.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -8.0).isActive = true
+        label.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -8.0).isActive = true
+        
+        /// set the width of the label to the width of the scrollView (-16 for 8.0 padding on each side)
+        label.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -16.0).isActive = true
+        
+        /// configure label: Zero lines + Word Wrapping
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+
+        /// initialize buttons with some basic size constaints
+        enterLandmarkDescriptionButton = UIButton.makeConstraintButton(view,
                                                                   alignment: UIConstants.ButtonContainerHorizontalAlignment.left,
                                                                   appearance: UIConstants.ButtonAppearance.imageButton(image: UIImage(named: "Describe")!),
                                                                   label: "Enter text to help you remember this landmark")
         
-        recordVoiceNoteButton = UIButton.makeImageButton(view,
+        recordVoiceNoteButton = UIButton.makeConstraintButton(view,
                                                          alignment: UIConstants.ButtonContainerHorizontalAlignment.right,
                                                          appearance: UIConstants.ButtonAppearance.imageButton(image: UIImage(named: "VoiceNote")!),
                                                          label: "Record audio to help you remember this landmark")
         
-        confirmAlignmentButton = UIButton.makeImageButton(view,
+        confirmAlignmentButton = UIButton.makeConstraintButton(view,
                                                           alignment: UIConstants.ButtonContainerHorizontalAlignment.center,
                                                           appearance: UIConstants.ButtonAppearance.imageButton(image: UIImage(named: "Align")!),
                                                           label: "Start \(ViewController.alignmentWaitingPeriod)-second alignment countdown")
         
+        /// create stack view for aligning and distributing bottom layer buttons
+        let stackView   = UIStackView()
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false;
+        
+        /// define horizonal, centered, and equal alignment of elements
+        /// inside the bottom stack
+        stackView.axis = NSLayoutConstraint.Axis.horizontal
+        stackView.distribution  = UIStackView.Distribution.equalSpacing
+        stackView.alignment = UIStackView.Alignment.center
+        
+        /// add elements to the stack
+        stackView.addArrangedSubview(enterLandmarkDescriptionButton)
+        stackView.addArrangedSubview(confirmAlignmentButton)
+        stackView.addArrangedSubview(recordVoiceNoteButton)
+        
+        scrollView.flashScrollIndicators()
+
+        /// size the stack
+        stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 450.0).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8.0).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8.0).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -UIConstants.buttonFrameWidth/7 * 2).isActive = true
+        
+        /// set function targets for the functions in this state
         if let parent: UIViewController = parent {
             enterLandmarkDescriptionButton.addTarget(parent,
                                      action: #selector(ViewController.showLandmarkInformationDialog),
@@ -86,10 +152,5 @@ class PauseTrackingController: UIViewController {
                                             action: #selector(ViewController.confirmAlignment),
                                             for: .touchUpInside)
         }
-        
-        // Do any additional setup after loading the view.
-        view.addSubview(enterLandmarkDescriptionButton)
-        view.addSubview(recordVoiceNoteButton)
-        view.addSubview(confirmAlignmentButton)
     }
 }
--- Clew/Extensions/UIButtonExtensions.swift
+++ Clew/Extensions/UIButtonExtensions.swift
@@ -65,4 +65,35 @@ extension UIButton {
         
         return button
     }
+    
+    static func makeConstraintButton(_ containerView: UIView,
+                                      alignment: UIConstants.ButtonContainerHorizontalAlignment,
+                                      appearance: UIConstants.ButtonAppearance,
+                                      label:String) -> UIButton {
+        let button = UIButton(type: .custom)
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
+        button.clipsToBounds = true
+        
+        /// enable use of constaint layout system
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        /// set width of button and constaint height to be equal to width
+        button.widthAnchor.constraint(equalToConstant: containerView.bounds.size.width / 3.75).isActive = true
+        button.heightAnchor.constraint(equalTo: button.widthAnchor).isActive = true
+        
+        /// apply appearance, either an image or a text field
+        switch appearance {
+        case .imageButton(let image):
+            button.setImage(image, for: .normal)
+        case .textButton(let label):
+            button.setTitle(label, for: .normal)
+            button.layer.borderWidth = 2
+            button.layer.borderColor = UIColor.white.cgColor
+        }
+        
+        button.accessibilityLabel = label
+        
+        return button
+    }
+    
 }
