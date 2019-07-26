eb8b8f2c771262ff16ff7fd0ecbe0374425791b6
hkRho <hrho@olin.edu>
2019-07-26 15:11:49 -0400

    Fix button for transition

--- Clew/Tutorial/TutorialVCs/TipsAndWarningsViewController.swift
+++ Clew/Tutorial/TutorialVCs/TipsAndWarningsViewController.swift
@@ -12,4 +12,16 @@ import SceneKit
 import FLAnimatedImage
 
 class TipsAndWarningsViewController: UIViewController {
+    func transitionToMainApp() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.window?.rootViewController?.dismiss(animated: false)
+        appDelegate.window = UIWindow(frame:UIScreen.main.bounds)
+        appDelegate.window?.makeKeyAndVisible()
+        appDelegate.window?.rootViewController = ViewController()
+    }
+    
+    /// Closes the viewcontroller
+    @IBAction func CloseTips(_ sender: UIButton) {
+        transitionToMainApp()
+    }
 }
