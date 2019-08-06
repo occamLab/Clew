5051c41495a133b50682c2c6af845420c2551e00
hkRho <hrho@olin.edu>
2019-08-06 16:39:50 -0400

    Settings and Feedback inside burger working

--- Clew/Controllers/PopOvers/BurgerMenuViewController.swift
+++ Clew/Controllers/PopOvers/BurgerMenuViewController.swift
@@ -14,6 +14,8 @@ class BurgerMenuViewController: UITableViewController, UIPopoverPresentationCont
     @IBOutlet weak var Tutorial: UIControl!
     @IBOutlet weak var Feedback: UIControl!
     
+    @IBOutlet weak var SettingsButton: UIButton!
+    @IBOutlet weak var FeedbackButton: UIButton!
     
     @IBAction func settingsTapped(sender: UIView) {
         print("YO")
@@ -33,8 +35,22 @@ class BurgerMenuViewController: UITableViewController, UIPopoverPresentationCont
         self.present(nav, animated: true, completion: nil)
     }
     
-    @IBAction func tutorialTapped(sender: AnyObject) {
-        print("hohoho")
+    @IBAction func feedbackButtonTapped(sender: UIButton) {
+        let storyBoard: UIStoryboard = UIStoryboard(name: "SettingsAndHelp", bundle: nil)
+        let popoverContent = storyBoard.instantiateViewController(withIdentifier: "Feedback") as! FeedbackViewController
+        popoverContent.preferredContentSize = CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+        let nav = UINavigationController(rootViewController: popoverContent)
+        nav.modalPresentationStyle = .popover
+        let popover = nav.popoverPresentationController
+        popover?.delegate = self
+        popover?.sourceView = self.view
+        popover?.sourceRect = CGRect(x: 0,
+                                     y: UIConstants.settingsAndHelpFrameHeight/2,
+                                     width: 0,
+                                     height: 0)
+        popoverContent.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: popoverContent, action: #selector(popoverContent.closeFeedback))
+//        suppressTrackingWarnings = true
+        self.present(nav, animated: true, completion: nil)
     }
     
 }
