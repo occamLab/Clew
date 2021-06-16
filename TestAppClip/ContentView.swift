322694e635db374debde84e6e9cce1fc2103d0d7
esme-abbot <eeabbot@gmail.com>
2021-06-16 15:54:01 -0400

    Add App Clip Target

---
+++ TestAppClip/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestAppClip
+//
+//  Created by occamlab on 6/16/21.
+//  Copyright © 2021 OccamLab. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct ContentView: View {
+    var body: some View {
+        recordPath()
+        
+        Text("THIS IS FOOD THIS IS BEANS")
+            .padding()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIKitClip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitClip
+//
+//  Created by occamlab on 6/16/21.
+//  Copyright © 2021 OccamLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
