d4a902266f6d0074106cf2a0ff6f3069ba69edcc
Declan <dketchum@olin.edu>
2021-06-24 16:03:44 -0400

    adding interactive orientation practice

--- Clew/Tutorial/TutorialTestViews.swift
+++ Clew/Tutorial/TutorialTestViews.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct TutorialScreen<Content: View>: View {
   let content: Content
@@ -142,36 +143,59 @@ struct OrientPhone: View {
 
 struct PracticeOrientPhone: View {
     @State private var score = 0
-    @ObservedObject private var
-        arData = ARData.shared
+    @State var lastSuccessSound = Date()
+    @State var lastSuccess = Date()
+    @ObservedObject private var arData = ARData.shared
     var body: some View{
         TutorialScreen {
             Text("score \(self.score)")
             if let transform = arData.transform {
-                var y = transform.columns.0.y
-                Text("y-component \(transform.columns.0.y)")
-                if -1... -0.95 ~= y {print("correct")}
-                
+                let y = transform.columns.0.y
+                Text("y-component \(y)")
+                if y < -0.9 {
+                    Text("correct")
+                }
             }
-            
+                
             Button("add 1") {score += 1} //add to score by holding phone correctly
             
-           /* insure ar session is running
-            if phone is alined
-                give haptic feedback
-            if phone is alined for 2 sec
-                make sucess sound
-                score += 1 */
-            
-            
             if score >= 3 {
-                Text("Yay!!!") //Play a happy sound
+                //AudioServicesPlaySystemSound(SystemSoundID(1057))
+                //UIAccessibility.post(notification: .announcement, argument: "Congratulations! You've completed the holding phone practice")
+                Text("Yay!!!")
                 NavigationLink(destination: FindPath()) {Text("Next")}
             }
                 
+            
+                
             if score < 3 {
                 NavigationLink(destination: FindPath()) {Text("Skip")}
+            }
             
+        }.onReceive(self.arData.objectWillChange) { newARData in
+                if let transform = arData.transform {
+                let y = transform.columns.0.y
+                if y < -0.4 && y > -0.7, -lastSuccessSound.timeIntervalSinceNow > 0.7 {
+                        AudioServicesPlaySystemSound(SystemSoundID(1057))
+                        lastSuccessSound = Date()
+                        UIAccessibility.post(notification: .announcement, argument: "Almost")
+                }
+                if y < -0.7 && y > -0.9, -lastSuccessSound.timeIntervalSinceNow > 0.5 {
+                        AudioServicesPlaySystemSound(SystemSoundID(1057))
+                        lastSuccessSound = Date()
+                        UIAccessibility.post(notification: .announcement, argument: "Almost")
+                }
+                if y < -0.9, -lastSuccessSound.timeIntervalSinceNow > 0.2 {
+                        AudioServicesPlaySystemSound(SystemSoundID(1057))
+                        lastSuccessSound = Date()
+                        UIAccessibility.post(notification: .announcement, argument: "WAY TO GO!")
+                }
+                if y < -0.9, -lastSuccess.timeIntervalSinceNow > 5 {
+                    print(score)
+                    score += 1
+                    lastSuccess = Date()
+                    UIAccessibility.post(notification: .announcement, argument: "Great")
+                }
             }
         }
     }
