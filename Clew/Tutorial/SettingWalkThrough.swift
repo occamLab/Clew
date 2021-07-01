a093c9b90cc50226117d4584afcd3668beba3ca1
Declan <dketchum@olin.edu>
2021-07-01 16:55:58 -0400

    making set up walk through

--- Clew/Tutorial/SettingWalkThrough.swift
+++ Clew/Tutorial/SettingWalkThrough.swift
@@ -8,14 +8,59 @@
 
 import SwiftUI
 
+
 struct SettingWalkThrough: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TutorialScreen{
+            Text("You can ajust all the settings from the settings tap in the main menu")
+            
+            TutorialNavLink(destination: setUnit()) {Text("Next")}
+        }
     }
 }
 
-struct SettingWalkThrough_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingWalkThrough()
+struct setUnit: View{
+    @State private var metric = true
+    var body: some View {
+        TutorialScreen{
+            Text("Distance Units")
+            
+            Button("Imperial", action:{ print("feet"); metric = false})
+            
+            Button("Metric", action: {print("meters"); metric = true})
+        }
     }
 }
+
+/*struct setColor: View{
+    @State private var crumbColor = "blue"
+    @State private var pathColor = "blue"
+    var body: some View {
+        TutorialScreen{
+            Text("Crumb and Path Color")
+            
+            Text("Set Crumb Color")
+            
+            Button("Red", action:{ print(crumbColor); crumbColor = "red"})
+            
+            Button("Green", action: {print("Green"); crumbColor = "green"})
+            
+            Button("Blue", action: {print("Blue")})
+            
+            Button("Random", action: {print("random")})
+            
+            Text("Set Path Color")
+            
+            Button("Red", action:{ print("Blue")})
+            
+            Button("Green", action: {print("Green")})
+            
+            Button("Blue", action: {print("Blue")})
+            
+            Button("Random", action: {print("random")})
+            
+            //render an image of the options
+            
+        }
+    }
+}*/
