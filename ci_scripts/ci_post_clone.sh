729d23cbece6fb4fd6aa69c09d6b6fba1a30a97f
Paul Ruvolo <paullundyruvolo@gmail.com>
2023-02-15 19:04:08 -0500

    trying a postclone

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1HC_b_21A005Lo98Eskn7yAqWefbw4FU9' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1HC_b_21A005Lo98Eskn7yAqWefbw4FU9" -O opencv2.framework.zip && rm -rf /tmp/cookies.txt
+unzip opencv2.framework.zip
+rm opencv2.framework.zip
